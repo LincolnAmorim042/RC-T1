f22151096fc9e09d41ecff2cb166b19225fe1347
Lincoln Amorim <54185019+lolincoln@users.noreply.github.com>
2022-07-21 01:02:29 -0400

    Add files via upload

--- server.py
+++ server.py
@@ -3,30 +3,27 @@ import socket
 import urllib3
 import sys
 import _thread
+import ssl
 
-# next create a socket object
+# cria o socket
 s = socket.socket()        
 print ("Socket successfully created")
  
-# reserve a port on your computer
+# reserva o port
 port = int(sys.argv[1])
 
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-# Next bind to the port
-# we have not typed any ip in the ip field
-# instead we have inputted an empty string
-# this makes the server listen to requests
-# coming from other computers on the network
+# binda o port
 s.bind(('', port))
 print ("socket binded to %s" %(port))
  
-# put the socket into listening mode
+# coloca o socket em listen
 s.listen(1)    
 print ("socket is listening")           
  
 def controlt(c):
-  # receive the url to be accessed and send the message back
+  # recebe o request
   site = c.recv(1024).decode()
   print (addr, 'connecting to:\n', site, '\n')
   method = ""
@@ -40,30 +37,47 @@ def controlt(c):
   while not(site[i].isspace()):
     url += site[i]
     i+=1
-  i=0
 
+  #trata o request
   match method:
+    case "CONNECT":
+      url1 = ""
+      i=0
+      while not(url[i]==':'):
+        url1 += url[i]
+        i+=1
+      i=0
+      s.connect((url1, 443))
+      request = b'GET', url1, 'HTTP/1.1\n\n'
+      c.send(request)
     case "GET":
       resp = http.request(method, url)
       c.send(resp.data)
-    #case "FLUSH":
-    #case "DELETE":
-    #case "INFO":
-    #case "MUDAR":
-  
-  # Close the connection with the client
+    case "HEAD":
+      resp = http.request(method, url)
+      c.send(resp.headers)
+    #case "POST":
+
+    #case "ADMIN"
+      #match url:
+        #case "FLUSH":
+        #case "DELETE":
+        #case "INFO":
+        #case "MUDAR":
+    case other:
+      c.send('Error 501 Not Implemented!')
+  i=0
+  # fecha a conexao com o cliente
   c.close()
 
-# a forever loop until we interrupt it or
-# an error occurs
+#loop
 while True:
- 
-# Establish connection with client.
+# abre a conexao com o cliente
   c, addr = s.accept()    
-  print ('Got connection from', addr )
+  print ('Got connection from', addr)
 
   http = urllib3.PoolManager()
-  
+  s = ssl.wrap_socket(s)
   _thread.start_new_thread(controlt,(c,))
  
   
