1f7f94759d336ed16932fb567351d3825b18f2d6
Lincoln Amorim <54185019+lolincoln@users.noreply.github.com>
2022-07-21 17:45:26 -0400

    Add files via upload

--- server.py
+++ server.py
@@ -1,27 +1,12 @@
 from curses.ascii import isspace
-import socket            
+import socket
 import urllib3
 import sys
 import _thread
 import ssl
+import certifi
 
-# cria o socket
-s = socket.socket()        
-print ("Socket successfully created")
- 
-# reserva o port
-port = int(sys.argv[1])
-
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-# binda o port
-s.bind(('', port))
-print ("socket binded to %s" %(port))
- 
-# coloca o socket em listen
-s.listen(1)    
-print ("socket is listening")           
- 
+#controle das threads
 def controlt(c):
   # recebe o request
   site = c.recv(1024).decode()
@@ -47,17 +32,26 @@ def controlt(c):
         url1 += url[i]
         i+=1
       i=0
-      s.connect((url1, 443))
-      request = b'GET', url1, 'HTTP/1.1\n\n'
-      c.send(request)
+      #s.connect((url1, 443))
+      #request = b'GET', url1, 'HTTP/1.1\n\n'
+      #c.send(request)
+      
+      url1 = "https://" + url1
+      http = urllib3.PoolManager(ca_certs=certifi.where())
+      resp = http.request('GET', url1)
+      c.send(resp.data)
     case "GET":
+      http = urllib3.PoolManager()
       resp = http.request(method, url)
       c.send(resp.data)
     case "HEAD":
+      http = urllib3.PoolManager()
       resp = http.request(method, url)
       c.send(resp.headers)
-    #case "POST":
-
+    case "POST":
+      http = urllib3.PoolManager(ca_certs=certifi.where())
+      resp = http.request(method, url, fields={'example': 'post'})
+      c.send(resp.data)
     #case "ADMIN"
       #match url:
         #case "FLUSH":
@@ -70,14 +64,29 @@ def controlt(c):
   # fecha a conexao com o cliente
   c.close()
 
+######################main###########################
+# cria o socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        
+print ("Socket successfully created")
+ 
+# reserva o port
+port = int(sys.argv[1])
+
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# binda o port
+s.bind(('', port))
+print ("socket binded to %s" %(port))
+ 
+# coloca o socket em listen
+s.listen(1)    
+print ("socket is listening")           
+
 #loop
 while True:
 # abre a conexao com o cliente
-  c, addr = s.accept()    
+  c, addr = s.accept()
   print ('Got connection from', addr)
 
-  http = urllib3.PoolManager()
-  s = ssl.wrap_socket(s)
+  #s = ssl.wrap_socket(s)
   _thread.start_new_thread(controlt,(c,))
- 
-  
