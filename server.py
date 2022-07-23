546f9ce0b34c84dc39a0f9cabfc5918ec48c28df
Lincoln Amorim <54185019+lolincoln@users.noreply.github.com>
2022-07-22 21:10:59 -0400

    Add files via upload

--- server.py
+++ server.py
@@ -9,11 +9,12 @@ BUFLEN=8192
 #controle das threads
 def controlt(c):
   # recebe o request
+  req = ""
   req = c.recv(BUFLEN).decode()
   print (addr, 'requesting:\n', req)
   method = ""
   url = ""
-  
+
   i=0
   while i<len(req) and not(req[i].isspace()):
     method += req[i]
@@ -24,14 +25,18 @@ def controlt(c):
     url += req[i]
     if i<len(req):
       i+=1
-  
+  i+=1
   if url=="":
     c.send(b'Incomplete Request')
     c.close()
     exit()
-  #trata o request
-  if not(req.find("1.1")==-1):
+
+  if not(req.find("1.1")==-1) and req.find("ADMIN")==-1:
     method = "GET"
+  if not(req.find("ADMIN")==-1):
+    url = url.upper()
+    req = req.upper()
+  #trata o request
   match method:
     case "GET":
       http = urllib3.PoolManager()
@@ -49,9 +54,22 @@ def controlt(c):
     #case "ADMIN":
       #match url:
         #case "FLUSH":
+          #limpa cache
         #case "DELETE":
+          #nome = ""
+          #while i<len(req) and not(req[i].isspace()):
+            #nome += req[i]
+            #i+=1
+          #apaga esta caceta
         #case "INFO":
+          #if not(req.find("INFO 0")==-1):
+            #tamanho atual e lista de objetos do cache
         #case "MUDAR":
+          #newsize = ""
+          #while i<len(req) and not(req[i].isspace()):
+            #newsize += req[i]
+            #i+=1
+          #tamanho cache = int(newsize)
     case other:
       c.send(b'Error 501 Not Implemented!')
   i=0
