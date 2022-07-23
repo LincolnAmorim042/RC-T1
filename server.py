7d45e1559bcb6676784a3aa15ceaa1cd66fa4477
Lincoln Amorim <54185019+lolincoln@users.noreply.github.com>
2022-07-23 01:56:57 -0400

    Add files via upload

--- server.py
+++ server.py
@@ -14,43 +14,44 @@ def controlt(c):
   req = c.recv(BUFLEN).decode()
   print (addr, 'requesting:\n', req)
 
-  reqsplit = req.split()
+  reqsp = req.split()
 
-  if len(reqsplit)==1:
+  if len(reqsp)==1:
     c.send(b'Incomplete Request')
     c.close()
     exit()
 
-  if "HTTP/1.1" in reqsplit and not("ADMIN" in reqsplit):
-    reqsplit[0] = "GET"
-  if "ADMIN" in reqsplit:
-    reqsplit[1] = reqsplit[1].upper()
+  if "HTTP/1.1" in reqsp and not("ADMIN" in reqsp):
+    reqsp[0] = "GET"
+  if "ADMIN" in reqsp:
+    reqsp[1] = reqsp[1].upper()
+  
   #trata o request
-  match reqsplit[0]:
+  match reqsp[0]:
     case "GET":
       http = urllib3.PoolManager()
-      resp = http.request(reqsplit[0], reqsplit[1])
+      resp = http.request(reqsp[0], reqsp[1])
       c.send(resp.data)
     case "HEAD":
       http = urllib3.PoolManager()
-      resp = http.request(reqsplit[0], reqsplit[1])
+      resp = http.request(reqsp[0], reqsp[1])
       val = str(resp.headers)
       c.send(val.encode())
     #case "POST":
       #http = urllib3.PoolManager()
-      #resp = http.request(reqsplit[0], reqsplit[1], fields={reqsplit[2]: reqsplit[3]})
+      #resp = http.request(reqsp[0], reqsp[1], fields={reqsp[2]: reqsp[3]})
       #c.send(resp.data) 
     #case "ADMIN":
-      #match reqsplit[1]:
+      #match reqsp[1]:
         #case "FLUSH":
           #limpa cache
         #case "DELETE":
-          #apaga reqsplit[2]
+          #apaga reqsp[2]
         #case "INFO":
-          #if reqsplit[2] == "0"):
-            #salva tamanho atual e lista de objetos do cache
+          #if reqsp[2] == "0": # salva tamanho atual e lista de objetos do cache
+          #if reqsp[2] == "1": # salva os não-expirados
         #case "MUDAR":
-          #tamanho cache = int(reqsplit[2])
+          #argv.c = int(reqsp[2])
     case other:
       c.send(b'Error 501 Not Implemented!')
   i=0
@@ -58,7 +59,6 @@ def controlt(c):
   c.close()
 
 ######################main###########################
-# cria o socket
 parser = argparse.ArgumentParser(prog='python3',usage='%(prog)s path [options]')
 parser.add_argument('-c', type=int, help="tamanho do cache em kb")
 parser.add_argument('-p', type=int, help="numero do port", required=True)
@@ -69,6 +69,7 @@ argv = parser.parse_args()
 
 port = argv.p
 
+# cria o socket
 s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)       
 try:
@@ -81,7 +82,7 @@ except socket.error:
 s.listen(1)    
 print ("socket is listening")           
 
-#loop
+# loop
 while True:
 # abre a conexao com o cliente
   c, addr = s.accept()
